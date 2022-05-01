9fa7c5cadc74b95bfbcdf972398c7657d9a3fa3b
Gadde <krgadde@amazon.com>
2022-05-01 08:32:30 -0500

    Adding files to git with error

--- src/main/java/com/shipmentEvents/handlers/EventHandler.java
+++ src/main/java/com/shipmentEvents/handlers/EventHandler.java
@@ -71,7 +71,7 @@ public class EventHandler implements RequestHandler<ScheduledEvent, String> {
     }
 
     public ShopifyShop connectToShopify(String subdomain) {
-        final String token4 = "shpss_sdkfhkjh134134141341344133412312345678";
+        final String token6 = "shpss_sdkfhkjh134134141341344133412312345678";
         final String token = "shpss_sdkfhkjh134134141341344133412312345678";
         final ShopifySdk shopifySdk = ShopifySdk.newBuilder()
              .withSubdomain(subdomain)
